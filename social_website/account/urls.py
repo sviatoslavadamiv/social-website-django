11f5a46d02c76aab78fd7e63e0302de6402bd4b8
sviatoslav <svatik5.adamiv@gmail.com>
2025-07-24 22:07:44 +0200

    Password change functionality added

--- social_website/account/urls.py
+++ social_website/account/urls.py
@@ -3,8 +3,21 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Login / Logout urls
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    # Change password url
+    path(
+        'password-change/',
+        auth_views.PasswordChangeView.as_view(),
+        name='password_change'
+    ),
+    path(
+        'password-change/done/',
+        auth_views.PasswordChangeDoneView.as_view(),
+        name='password_change_done'
+    ),
 
     path('', views.dashboard, name='dashboard'),
 ]
