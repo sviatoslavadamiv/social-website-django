f8705357e9260a458c16698c70cdd6ea38e05bf9
sviatoslav <svatik5.adamiv@gmail.com>
2025-07-24 22:43:51 +0200

    User registration functionality added

--- social_website/account/urls.py
+++ social_website/account/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
 ]
