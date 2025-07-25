77afbdc969e7441f6cb5fae01063a50c6bfd8619
sviatoslav <svatik5.adamiv@gmail.com>
2025-07-26 00:08:53 +0200

    Added messages to edit view

--- social_website/account/views.py
+++ social_website/account/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile
+from django.contrib import messages
 
 # def user_login(request):
 #     if request.method == 'POST':
@@ -80,6 +81,12 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(
+                request,
+                'Profile updated successfully'
+            )
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
