74670084ea4d6efaccc2e9abbe8152079d4f829b
sviatoslav <svatik5.adamiv@gmail.com>
2025-07-27 20:45:47 +0200

    Follow functionality added

--- social_website/account/views.py
+++ social_website/account/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.views.decorators.http import require_POST
+
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
-from .models import Profile
+from .models import Profile, Contact
 from django.contrib import messages
 from django.contrib.auth import authenticate, get_user_model, login
 from django.shortcuts import get_object_or_404, render
@@ -102,3 +104,26 @@ def user_detail(request, username):
             'user': user,
         }
     )
+
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(
+                    user_from=request.user,
+                    user_to=user,
+                )
+            else:
+                Contact.objects.filter(
+                    user_from=request.user,
+                    user_to=user,
+                ).delete()
+            return JsonResponse({'status': 'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status': 'error'})
+    return JsonResponse({'status': 'error'})
