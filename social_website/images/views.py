da9bbfd519c239c640181f199f2e0ecaefe4a8b3
sviatoslav <svatik5.adamiv@gmail.com>
2025-07-28 00:40:00 +0200

    Ranking of images by views created

--- social_website/images/views.py
+++ social_website/images/views.py
@@ -54,6 +54,8 @@ def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     # Increment total image views by 1
     total_views = r.incr(f"image:{image.id}:views")
+    # Increment image ranking by 1
+    r.zincrby('image_ranking', 1, image.id)
     return render(
         request,
         "images/image/detail.html",
@@ -113,5 +115,29 @@ def image_list(request):
         {
             'section': 'images',
             'images': images,
+        }
+    )
+
+@login_required
+def image_ranking(request):
+    # Get image ranking dictionary
+    image_ranking = r.zrange(
+        'image_ranking', 0, -1,
+        desc=True
+    )[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    # Get most viewed images
+    most_viewed = list(
+        Image.objects.filter(
+            id__in=image_ranking_ids
+        )
+    )
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(
+        request,
+        'images/image/ranking.html',
+        {
+            'section': 'images',
+            'most_viewed': most_viewed,
         }
     )
