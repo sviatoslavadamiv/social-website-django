7109470a2345ed7db42f6819f5b72cbbc44950ba
sviatoslav <svatik5.adamiv@gmail.com>
2025-07-27 15:38:39 +0200

    Added functionality to post content from other websites by bookmarking images from external websites and share them on our site

--- social_website/images/views.py
+++ social_website/images/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from .forms import ImageCreateForm
 
-# Create your views here.
+@login_required
+def image_create(request):
+    if request.method == 'POST':
+        # Form is sent
+        form = ImageCreateForm(data=request.POST)
+        if form.is_valid():
+            # From data is valid
+            cd = form.cleaned_data
+            new_image = form.save(commit=False)
+            # Assign current user to the item
+            new_image.user = request.user
+            new_image.save()
+            messages.success(request, 'Image added successfully')
+            # Redirect to new created item detail view
+            return redirect(new_image.get_absolute_url())
+    else:
+        # Build form with data provided by the bookmarklet via GET
+        form = ImageCreateForm(data=request.GET)
+    return render(
+        request,
+        'images/image/create.html',
+        {
+            'section': 'images',
+            'form': form,
+        }
+    )
